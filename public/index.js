9937adf745177a8499f5788b07599cd876724449
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2021-08-14 14:42:58 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 function sendToPage(page) {
     var request = new XMLHttpRequest();
-    request.open("GET", "192.168.86.30:3000/" + page, false);
+    request.open("GET", "/" + page, false);
     request.send(null);
 }
