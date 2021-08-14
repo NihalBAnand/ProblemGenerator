65979663e78f6b54c783f9ca5e314ffdd911c717
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2021-08-14 14:34:28 -0500

    added about routing

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/about.html'))
+})
+
 app.listen(3000, () => {
     console.log("Listening on port 3000.");
 })
