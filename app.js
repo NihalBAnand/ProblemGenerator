19727041d5f1a90f0ff548e0c95e05ad1fb1243b
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2021-08-16 22:36:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get('/get-problem', (req, res) => {
         
         var weight = Math.round(Math.random() * 4);
         if(weight == 0 || priority.length == 0) {
-            console.log(plebes[0]);
+            console.log(plebes[0][0]);
             out = plebes[getRandomInt(0, plebes.length - 1)];
         }
         else {
